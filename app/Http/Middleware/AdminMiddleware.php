06273dda7d9c64c4e95bcfc87eeece858272dae2
Mnjar <fajarftr2605@gmail.com>
2024-06-11 01:26:37 +0700

    add AdminMiddleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::check())
+        {
+            if(Auth::user()->role != 'admin')
+            {
+                return redirect('/admin-home');
+            }
+        }
+        else
+        {
+            return redirect ('/user-home');
+        }
+
+        return $next($request);
+    }
+}
