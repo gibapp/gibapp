5633558bb7fe34aa3af96642dcfecb8d5ead627f
chrisprojs <christiananggaresta20@gmail.com>
2024-06-11 12:49:13 +0700

    Fix: lost page doesn't direct to home page

--- resources/views/pages/lost.blade.php
+++ resources/views/pages/lost.blade.php
@@ -34,8 +34,8 @@
         </button>
         <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
             <div class="navbar-nav navbar-choice">
-                <a href="{{ url('/admin') }}" class="nav-item nav-link">Home</a>
-                <a href="{{ url('/admin-lost') }}" class="nav-item nav-link active">Lost</a>
+                <a href="{{ url('/') }}" class="nav-item nav-link">Home</a>
+                <a href="{{ url('/lost') }}" class="nav-item nav-link active">Lost</a>
             </div>
             <div class="d-flex m-3 me-0">
                 <a href="{{ url('/admin') }}" class="my-auto">
