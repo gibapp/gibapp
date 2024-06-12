6833e9fe282b3d0ae751c1bc4ec810ec50143f64
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2024-06-12 20:14:50 +0700

    fix: class table naming issue

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Items;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
+    protected $table = 'categories'; 
     protected $fillable = ['category_name'];
 
-    public function items()
+    public function items(): HasMany
     {
         return $this->hasMany(Items::class, 'category_id');
     }
--- app/Models/Items.php
+++ app/Models/Items.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Items extends Model
 {
+    protected $table = 'lost_items';
     protected $fillable = ['item_name', 'image', 'description', 'finders_name', 'found_location', 'date', 'category_id'];
 
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
